35b91f074694381e3caa446b8654f954519dadbc
gaeul <chlrkdmf0805@gmail.com>
2022-08-02 10:15:08 +0900

    액티비티로 화면 전환하기

---
+++ And_0801_ativityOne/app/src/main/java/com/cookandroid/and_0801_ativityone/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.cookandroid.and_0801_ativityone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+    Button btnSecond;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        setTitle("최가을 두번째 화면");
+
+        btnSecond = findViewById(R.id.btnSecond);
+
+        btnSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
---
+++ And_0801_ativityOne/app/src/main/java/com/cookandroid/and_0801_ativityone/Third_activity.java
@@ -0,0 +1,13 @@
+package com.cookandroid.and_0801_ativityone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Third_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
