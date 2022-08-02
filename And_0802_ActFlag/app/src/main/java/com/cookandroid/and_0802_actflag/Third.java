bec9eb48f53f4dde03a8fc470c18108cb51f563f
gaeul <chlrkdmf0805@gmail.com>
2022-08-02 13:06:37 +0900

    인텐트 플래그를 사용해 메인화면 두번째화면 세번째화면 번갈아 띄우기

---
+++ And_0802_ActFlag/app/src/main/java/com/cookandroid/and_0802_actflag/Third.java
@@ -0,0 +1,36 @@
+package com.cookandroid.and_0802_actflag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Third extends AppCompatActivity {
+
+    Button btn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        btn3 = findViewById(R.id.btn3);
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent in = new Intent(getApplicationContext(), MainActivity.class);
+//                in.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+//                startActivity(in);
+
+                Intent in = new Intent(getApplicationContext(), MainActivity.class);
+                in.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(in);
+                finish();
+            }
+        });
+
+    }
+}
