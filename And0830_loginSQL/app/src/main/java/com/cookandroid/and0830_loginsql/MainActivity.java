fe959caeee9e95dc5da2acf3b9ba1e4f7e629450
gaeul <chlrkdmf0805@gmail.com>
2022-09-01 18:38:07 +0900

    회원가입, 로그인, 로그인 정보 xml 코딩

--- And0830_loginSQL/app/src/main/java/com/cookandroid/and0830_loginsql/MainActivity.java
+++ And0830_loginSQL/app/src/main/java/com/cookandroid/and0830_loginsql/MainActivity.java
@@ -2,14 +2,22 @@ package com.cookandroid.and0830_loginsql;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edtId, edtPw;
     Button btnLogin, btnSign;
+    SQLiteDatabase sqlDB;
+    MyDBHelper myHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +27,34 @@ public class MainActivity extends AppCompatActivity {
         edtPw = findViewById(R.id.edtPw);
         btnLogin = findViewById(R.id.btnLogin);
         btnSign = findViewById(R.id.btnSign);
+        myHelper = new MyDBHelper(this);
+
+        btnSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(in);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userID = edtId.getText().toString().trim();
+                String userPass = edtPw.getText().toString().trim();
+                sqlDB = myHelper.getReadableDatabase();
+                Cursor cursor;
+                cursor = sqlDB.rawQuery("SELECT userID, userPass FROM user where userID='"+userID+"' and userPass='"+userPass+"';", null);
+                
+                if(cursor.moveToFirst()){
+                    Intent in = new Intent(MainActivity.this, LoginOKActivity.class);
+                    in.putExtra("userID", userID);
+                    in.putExtra("userPass", userPass);
+                    startActivity(in);
+                }else{
+                    Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
